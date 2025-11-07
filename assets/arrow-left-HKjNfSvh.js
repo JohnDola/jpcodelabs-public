40ac0eb8edbb9a664e78e1e87b68ce7bf0d87031
JohnDola <j.m.dola1990@gmail.com>
2025-11-07 13:12:31 +0000

    Deploying to main from @ JohnDola/jpcodelabs-code@5bc0975cbb1dee38141863d3eb5c950260557e05 🚀

--- assets/arrow-left-HKjNfSvh.js
+++ assets/arrow-left-HKjNfSvh.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-C15h9oYQ.js";/**
+import{c as e}from"./index-B2e5fAaP.js";/**
  * @license lucide-vue-next v0.300.0 - ISC
  *
  * This source code is licensed under the ISC license.
