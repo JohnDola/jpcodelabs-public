2b99b17293b1df26a3328a7f0ab4a140a894df56
JohnDola <j.m.dola1990@gmail.com>
2025-11-07 14:43:48 +0000

    Deploying to main from @ JohnDola/jpcodelabs-code@53fa7f7e82e31d3a0c466c394f886383e65fcc26 🚀

--- assets/arrow-left-BC6Gijzu.js
+++ assets/arrow-left-BC6Gijzu.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DUdpsARB.js";/**
+import{c as e}from"./index-BTPd_V61.js";/**
  * @license lucide-vue-next v0.300.0 - ISC
  *
  * This source code is licensed under the ISC license.
