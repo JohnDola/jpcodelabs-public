a1f48dacb88f439a63d96bb4ef14ba902a8eb4f9
JohnDola <j.m.dola1990@gmail.com>
2026-07-27 15:15:53 +0000

    Deploying to main from @ JohnDola/jpcodelabs-code@6bcbaf9036b4264a91a07e691adfa282bc646df7 🚀

--- assets/usePlausible-CCEpyrrn.js
+++
@@ -1,6 +0,0 @@
-import{c as n}from"./index-CcaxajZy.js";/**
- * @license lucide-vue-next v0.300.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const u=n("MoveRightIcon",[["path",{d:"M18 8L22 12L18 16",key:"1r0oui"}],["path",{d:"M2 12H22",key:"1m8cig"}]]);function a(){function i(o,e){typeof window<"u"&&window.plausible&&window.plausible(o,e?{props:e}:void 0)}return{trackEvent:i}}export{u as M,a as u};
